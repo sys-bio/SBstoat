a9872842105926d573d0c8abb3308e7175913c0d
Joseph Hellerstein <jlheller@uw.edu>
2020-11-30 10:51:11 -0800

    Updated profile processing.

--- profile.sh
+++ profile.sh
@@ -1,13 +1,11 @@
-#! /bin/bash
 # Profiles codes that use SBstoat. $1 is the file or use the default benchmark.
-PATH="tests/benchmarkModelFitter.py"
+PPATH="tests/benchmarkModelFitter.py"
 if [ $# -eq 1 ]; then
-    PATH="$1"
+    PPATH="$1"
 fi
-echo "Profiling file ${PATH}"
-python -m ${PATH} > profile.csv
+echo "Profiling file ${PPATH}"
+python -m cProfile ${PPATH} > profile.csv
+exit()
 echo " ncalls  tottime  percall  cumtime  percall filename:lineno(function)" > result.csv
-grep namedTimeseries profile.csv >> result.csv
-grep timeseriesPlotter profile.csv >> result.csv
-grep modelFitter profile.csv >> result.csv
-grep "_helper" profile.csv >> result.csv
+grep "^  *[1-9].*:" profile.csv >> result.csv
+sed 's/ /,/g' result.csv | sed 's/,,/,/g' | sed 's/,,/,/g' | sed 's/,,/,/g' | sed 's/^,//' > result.csv
